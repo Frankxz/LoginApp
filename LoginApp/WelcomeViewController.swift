45637ecd02db0120108c7566a0b570b344958e30
Miller <guf302010@gmail.com>
2021-07-27 14:01:17 +0600

    Gradient + full screen on the welcomeVC

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -8,13 +8,25 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-    var username = ""
-    @IBOutlet weak var logOutButton: UIButton!
     @IBOutlet weak var label: UILabel!
+    var username = ""
     
+    private let primaryColor = UIColor(
+        red: 250/255,
+        green: 112/255,
+        blue: 154/255,
+        alpha: 1
+    )
+    private let secondaryColor = UIColor(
+        red: 254/255,
+        green: 225/255,
+        blue: 64/255,
+        alpha: 1
+    )
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
         label.text = "Hello, dear " + username + "!"
     
     }
